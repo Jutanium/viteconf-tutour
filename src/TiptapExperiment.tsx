5870e19e40d1d6c60c501a8ee6cc6b168aa32b64
Jutanium <danjutan@gmail.com>
2022-07-24 11:48:46 -0400

    experiment with custom node

--- src/TiptapExperiment.tsx
+++ src/TiptapExperiment.tsx
@@ -1,8 +1,62 @@
-import { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 import StarterKit from "@tiptap/starter-kit";
 import BubbleMenu from "@tiptap/extension-bubble-menu";
 import { createEditorTransaction, createTiptapEditor } from "solid-tiptap";
-import { Editor } from "@tiptap/core";
+import { Editor, Node, mergeAttributes } from "@tiptap/core";
+
+const [count, setCount] = createSignal(0);
+
+const nodeExtension = Node.create({
+  name: "codeLinkBlock",
+  group: "block",
+  atom: true,
+  parseHTML() {
+    return [{ tag: "code-link" }];
+  },
+  renderHTML({ HTMLAttributes }) {
+    return ["code-link", mergeAttributes(HTMLAttributes)];
+  },
+  addAttributes() {
+    return {
+      multiplier: {
+        default: 1,
+      },
+    };
+  },
+  addNodeView() {
+    return ({ node, HTMLAttributes, getPos, editor }) => {
+      function incMultiplier() {
+        if (typeof getPos === "function") {
+          console.log("here");
+          editor
+            .chain()
+            .command(({ tr }) => {
+              const position = getPos();
+              const currentNode = tr.doc.nodeAt(position);
+
+              tr.setNodeMarkup(position, undefined, {
+                ...currentNode?.attrs,
+                multiplier: currentNode?.attrs?.multiplier + 1 || 1,
+              });
+
+              return true;
+            })
+            .run();
+        }
+      }
+      return {
+        dom: (
+          <div class="border border-blue-500 gap-4">
+            <button onClick={() => setCount((c) => c + 1)}>
+              {count() * node.attrs.multiplier}
+            </button>
+            <button onClick={incMultiplier}>Mult</button>
+          </div>
+        ),
+      };
+    };
+  },
+});
 
 const TiptapExperiment: Component<{}> = (props) => {
   const element: HTMLElement = (<div></div>) as HTMLElement;
@@ -20,13 +74,11 @@ const TiptapExperiment: Component<{}> = (props) => {
     },
     get extensions() {
       return [
+        nodeExtension,
         StarterKit.configure({
           heading: {
             // levels: [2, 3, 4, 5, 6],
           },
-        }),
-        BubbleMenu.configure({
-          element: bubbleMenuElement(),
         }),
       ];
     },
@@ -45,24 +97,33 @@ const TiptapExperiment: Component<{}> = (props) => {
     editor?.isActive("bold")
   );
 
-  function bubbleMenuElement() {
-    return (
-      <div>
+  function boldText() {
+    editor().chain().focus().toggleBold().run();
+  }
+
+  return (
+    <div class="flex flex-col">
+      <div class="flex w-full gap-2">
         <button
           class={`${isBold() ? "bg-gray-200" : "bg-white"}`}
           onClick={boldText}
         >
           Bold
         </button>
+        <button
+          onClick={() =>
+            editor().commands.insertContent(
+              "<code-link multiplier=2></code-link>"
+            )
+          }
+        >
+          Insert
+        </button>
+        <button onClick={() => console.log(editor().getHTML())}>Print</button>
       </div>
-    ) as HTMLElement;
-  }
-
-  function boldText() {
-    editor().chain().focus().toggleBold().run();
-  }
-
-  return <div>{element}</div>;
+      {element}
+    </div>
+  );
 };
 
 export default TiptapExperiment;
