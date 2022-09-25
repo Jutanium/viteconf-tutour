79eeea18ed70900e427b39d3e96ad89994bd5e71
Dan Jutan <danjutan@gmail.com>
2022-09-25 18:08:37 -0400

    fix auto indent

--- src/codemirror/baseExtensions.ts
+++ src/codemirror/baseExtensions.ts
@@ -1,27 +1,35 @@
 import {
-  lineNumbers,
-  highlightActiveLineGutter,
-  highlightSpecialChars,
-  drawSelection,
-  dropCursor,
-  rectangularSelection,
-  crosshairCursor,
-  highlightActiveLine,
-  EditorView,
-  keymap,
-} from "@codemirror/view";
-export { EditorView } from "@codemirror/view";
+  autocompletion,
+  closeBrackets,
+  closeBracketsKeymap,
+  completionKeymap,
+} from "@codemirror/autocomplete";
+import {
+  defaultKeymap,
+  history,
+  historyKeymap,
+  indentWithTab,
+} from "@codemirror/commands";
+import {
+  bracketMatching,
+  foldGutter,
+  foldKeymap,
+  indentOnInput,
+} from "@codemirror/language";
+import { highlightSelectionMatches, searchKeymap } from "@codemirror/search";
 import { EditorState } from "@codemirror/state";
 import {
-  foldGutter,
-  indentOnInput,
-  syntaxHighlighting,
-  defaultHighlightStyle,
-  bracketMatching,
-} from "@codemirror/language";
-import { history, indentWithTab } from "@codemirror/commands";
-import { highlightSelectionMatches } from "@codemirror/search";
-import { closeBrackets, autocompletion } from "@codemirror/autocomplete";
+  crosshairCursor,
+  drawSelection,
+  dropCursor,
+  EditorView,
+  highlightActiveLine,
+  highlightActiveLineGutter,
+  highlightSpecialChars,
+  keymap,
+  rectangularSelection,
+} from "@codemirror/view";
+export { EditorView } from "@codemirror/view";
 
 export default [
   highlightActiveLineGutter(),
@@ -32,8 +40,16 @@ export default [
   dropCursor(),
   EditorState.allowMultipleSelections.of(true),
   indentOnInput(),
-  keymap.of([indentWithTab]),
-  // bracketMatching(),
+  bracketMatching(),
+  keymap.of([
+    indentWithTab,
+    ...closeBracketsKeymap,
+    ...defaultKeymap,
+    ...searchKeymap,
+    ...historyKeymap,
+    ...foldKeymap,
+    ...completionKeymap,
+  ]),
   closeBrackets(),
   autocompletion(),
   rectangularSelection(),
